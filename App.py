04a098aea4077e9979582525731d39b4430edac0
Piyush Pande <pandepiyush1371@gmail.com>
2023-02-20 12:22:43 +0530

    Updated to version 9

--- App.py
+++ App.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
   
 @app.route('/')
 def hello():
-    return "Welcome to the flask demo app v7!"
+    return "Welcome to the flask demo app v9!"
   
   
 if __name__ == "__main__":
