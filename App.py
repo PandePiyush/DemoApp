e07840efbf3c5362e8e109fc2902c23718031fff
Piyush Pande <pandepiyush1371@gmail.com>
2023-02-21 15:19:52 +0530

    We are Team

--- App.py
+++ App.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
   
 @app.route('/')
 def hello():
-    return "SSBT College TE-A Section is Ravas..."
+    return "SSBT College TE-A Section is Ravas!"
   
   
 if __name__ == "__main__":
