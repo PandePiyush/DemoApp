26af9d69c82d29e342174d930eafe234b2e82107
Piyush Pande <pandepiyush1371@gmail.com>
2023-02-25 15:32:25 +0530

    MY PROJECT WORK IS OVER NOW YOU PEOPLE CAN PROCEED FURTHER

--- App.py
+++ App.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
   
 @app.route('/')
 def hello():
-    return "My Future is an ideal projection of what i envision now!"
+    return "SSBT TE-B Section is Ravas!"
   
   
 if __name__ == "__main__":
